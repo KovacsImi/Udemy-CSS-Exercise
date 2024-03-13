cc85929fe859e8361a51928907cb9c63e3bdb802
KovácsImi <imre90@gmail.com>
2024-03-13 10:35:17 +0100

    add shared JS file

--- shared.js
+++ shared.js
@@ -2,15 +2,30 @@ const backdrop = document.querySelector('.backdrop')
 const modal = document.querySelector(".modal")
 const selectPlanButtons = document.querySelectorAll('.plan button')
 const modalNoButton = document.querySelector(".modal__action--negative")
+const toggleButton = document.querySelector(".toggle-button")
+const mobileNav = document.querySelector(".mobile-nav")
 
-for(const button of selectPlanButtons){
-    button.addEventListener('click', function(){
+function closeModal() {
+    backdrop.style.display = "none"
+    modal.style.display = "none"
+}
+
+for (const button of selectPlanButtons) {
+    button.addEventListener('click', function () {
         modal.style.display = "block"
         backdrop.style.display = 'block'
     })
 }
 
-modalNoButton.addEventListener('click', function (){
-    modal.style.display = "none"
-    backdrop.style.display = "none"
+backdrop.addEventListener("click", function () {
+    mobileNav.style.display = "none"
+    closeModal()
+})
+
+modalNoButton.addEventListener('click', closeModal
+)
+
+toggleButton.addEventListener("click", function () {
+    mobileNav.style.display = "block"
+    backdrop.style.display = "block"
 })
