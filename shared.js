46157df98d48618e694d9aa91e672fd7e3a34b96
KovácsImi <imre90@gmail.com>
2024-03-14 12:13:46 +0100

    add hamburger menu to customers and packages sides

--- shared.js
+++ shared.js
@@ -1,38 +1,41 @@
-const backdrop = document.querySelector('.backdrop')
-const modal = document.querySelector(".modal")
-const selectPlanButtons = document.querySelectorAll('.plan button')
-const modalNoButton = document.querySelector(".modal__action--negative")
-const toggleButton = document.querySelector(".toggle-button")
-const mobileNav = document.querySelector(".mobile-nav")
+let backdrop = document.querySelector('.backdrop')
+let modal = document.querySelector(".modal")
+let selectPlanButtons = document.querySelectorAll('.plan button')
+let modalNoButton = document.querySelector(".modal__action--negative")
+let toggleButton = document.querySelector(".toggle-button")
+let mobileNav = document.querySelector(".mobile-nav")
 
-function closeModal() {
-    modal.classList.remove('open');
-    backdrop.classList.remove('open');
-    // backdrop.style.display = "none"
-    // modal.style.display = "none"
-}
+// for (const button of selectPlanButtons) {
+//     button.addEventListener('click', function () {
+//         modal.classList.add('open')
+//         backdrop.classList.add('open')
+//     })
+// }
 
-for (const button of selectPlanButtons) {
-    button.addEventListener('click', function () {
-        // modal.style.display = "block"
-        // backdrop.style.display = 'block'
-        modal.classList.add('open')
-        backdrop.classList.add('open')
-    })
+for (const element of selectPlanButtons) {
+    element.addEventListener("click", function() {
+        modal.classList.add("open");
+        backdrop.classList.add("open");
+    });
 }
 
 backdrop.addEventListener("click", function () {
-    // mobileNav.style.display = "none"
     mobileNav.classList.remove('open');
     closeModal()
 })
 
-modalNoButton.addEventListener('click', closeModal
-)
+if(modalNoButton){
+    modalNoButton.addEventListener('click', closeModal)
+}
+
+function closeModal() {
+    if(modal){
+        modal.classList.remove('open');
+    }
+    backdrop.classList.remove('open');
+}
 
 toggleButton.addEventListener("click", function () {
     mobileNav.classList.add('open');
     backdrop.classList.add('open');
-    // mobileNav.style.display = "block"
-    // backdrop.style.display = "block"
 })
